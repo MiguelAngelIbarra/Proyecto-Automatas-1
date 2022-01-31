d3fbde80b78739e5e7ff72ce7872f8df046efd7d
MiguelAngelIbarra <89610156+MiguelAngelIbarra@users.noreply.github.com>
2022-01-31 10:00:34 -0600

    Add files via upload

---
+++ Prueba.cs
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib>
+#include <graphics.h>
+
+float volumen, radio, altura;
+char opcion;
+int a,b,c,d;
+float x,y;
+void main()
+{
+    printf("\nIngrese el radio del cilindro", volumen, radio, altura);
+    scanf("%f" , &radio);
+    printf("\nIngrese la altura");
+    scanf("%f", &altura);
+    printf("\nAltura = %f Radio = %f", altura, radio);
+    if (altura > 100)
+    {
+        for (i = 0; i < 100; i++)
+        {
+            if (i == 5)
+            {
+                printf("\ni es igual a 5");
+            }
+        }
+        printf("\nMayor que 100");
+    }
+    else if (altura < 0)
+    {
+        while (i < 100)
+            switch (d)
+            {
+                case 1:
+                    if (i == 1)
+                    {
+                        printf("\ni es igual a 1");
+                    }
+                    break;
+                case 2:
+                
+                case 3:
+                    if (i == 2)
+                    {
+                        printf("\ni es igual a 2");
+                    }
+                    break;
+                    default:
+                    printf("\nError");
+            }
+        printf("\nNegativa");
+    }
+    else
+    {
+        printf("\nPositiva");
+    }
+}
+			
+		
+			
+		
+		
+
+		
+
+
